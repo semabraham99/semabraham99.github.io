90ed728e3770e22e8d8d4c8c60ae2aac3e82f65a
semabraham99 <semaw190699@gmail.com>
2022-12-05 11:44:23 +0700

    tambah function button

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
-let pageContent = {
-    page: 1,
-    soal : "apakah pertanyaan itu ?",
-    jawabanA: "jawaban 1",
-    jawabanB: "jawaban 2",
-    nextPage : function () {
-        this.page = this.page + 1
-        console.log ("level up")
+let page = 0
+
+
+const key = [1,2,1,1]
+const answerA = document.getElementById('buttonA')
+const answerB = document.getElementById('buttonB')
+
+function cekJawaban (id) {
+    console.log("di click")
+    if (page >= 5){
+        page = 0
+    }
+
+    if (id === key[page]) {
+        jawabanBenar()
+        setTimeout(()=> {nextPage()}, 500)       
+    }
+
+    else {
+        jawabanSalah()
+        setTimeout(()=> {nextPage()}, 500)
     }
 }
 
-const answerA = document.getElementById('answerA')
+function jawabanBenar(){
+    if (key[page]===1){
+        answerA.style.backgroundColor = "blue"
+    }
+    else{
+        answerB.style.backgroundColor = "blue"
+    }
+}
 
-// ubah text dan warna answer A
-answerA.textContent = pageContent.jawabanA
-answerA.style.backgroundColor = "red"
+function jawabanSalah(){
+    if (key[page]===1){
+        answerB.style.backgroundColor = "red"
+    }
+    else{
+        answerA.style.backgroundColor = "red"
+    }
+}
+
+function nextPage() {
+    page = page + 1
+    console.log ("level up")
+    resetView()
+}
+
+function resetView() {
+    answerA.style.backgroundColor = "#0e9e95"
+    answerB.style.backgroundColor = "#0e9e95"
+}
+
+// // ubah text dan warna answer A
+    // answerA.textContent = pageContent.jawabanA
+    // answerA.style.backgroundColor = "red"
